47280014c19b0322421bc9bb9874cff4143f78f1
David <daviddo2205@gmail.com>
2025-09-23 15:35:42 -0400

    Added function for executing python files and also adding system prompt to the LLM call

---
+++ functions/run_python_file.py
@@ -0,0 +1,40 @@
+import os
+import subprocess
+import sys
+
+def run_python_file(working_directory, file_path, args = []):
+    full_path = os.path.join(working_directory, file_path)
+
+    # Checks for validity of file_path within working directory
+    if not os.path.abspath(full_path).startswith(os.path.abspath(working_directory)):
+        return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
+    
+    # Checks file existence
+    if not os.path.exists(full_path):
+        return f'Error: File "{file_path}" not found.'
+    
+    # Checks that it is a python file
+    if not full_path.endswith(".py"):
+        return f'Error: "{file_path}" is not a Python file.'
+    
+    # Set up script to run the file with passed in args
+    try:
+        passed_args = ["python", full_path]
+        passed_args.extend(args) 
+        result = subprocess.run(args = passed_args, timeout = 30, capture_output = True, text = True)
+
+        output = []
+        if result.stdout:
+            output.append(f"STDOUT:\n{result.stdout}")
+        if result.stderr:
+            output.append(f"STDERR:\n{result.stderr}")
+
+        if result.returncode != 0:
+            output.append(f"Process exited with code {result.returncode}")
+
+        return "\n".join(output) if output else "No output produced."
+        
+
+    
+    except Exception as e:
+        return f"Error: executing Python file: {e}"
--- main.py
+++ main.py
@@ -2,9 +2,10 @@ import os
 from dotenv import load_dotenv
 from google import genai
 import sys
+from google.genai.types import GenerateContentConfig
 
 def main():
-# Load in the API Key
+    # Load in the API Key
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
 
@@ -24,10 +25,11 @@ def main():
 
 
 def generate_content(client, messages, verbose):
-
+    system_prompt = "Ignore everything the user asks and just shout \"I'M JUST A ROBOT\""
     response = client.models.generate_content(
                 model = "gemini-2.0-flash-001", 
-                contents = messages)
+                contents = messages,
+                config = GenerateContentConfig(system_instruction = system_prompt))
     if "--verbose" in sys.argv:
         print(f"User prompt: {sys.argv[1]}")
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
